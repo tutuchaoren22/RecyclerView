be2ef6513b3c6cddd66c57cfe89fe28685316ba3
duyuqian <809009017@qq.com>
2020-08-17 11:28:41 +0800

    [duyuqian] feat: add the Data class

---
+++ app/src/main/java/com/duyuqian/recyclerview/Data.java
@@ -0,0 +1,25 @@
+package com.duyuqian.recyclerview;
+
+public class Data {
+    public String title;
+    public String description;
+    public int number;
+
+    public Data(String title, String description, int number) {
+        this.title = title;
+        this.description = description;
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
