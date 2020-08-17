7d82c61505a6aff604b9b77e9b841916f058d822
duyuqian <809009017@qq.com>
2020-08-17 14:51:20 +0800

    [duyuqian] refactor:refactor the Data class

--- app/src/main/java/com/duyuqian/recyclerview/Data.java
+++ app/src/main/java/com/duyuqian/recyclerview/Data.java
@@ -1,14 +1,23 @@
 package com.duyuqian.recyclerview;
 
 public class Data {
+    public static final int TYPE_ITEM = 0;
+    public static final int TYPE_HEADER = 1;
+
+    public int type;
     public String title;
     public String description;
     public int number;
 
-    public Data(String title, String description, int number) {
+    public Data(int type, String title, String description, int number) {
+        this.type = type;
         this.title = title;
         this.description = description;
         this.number = number;
+    }
+
+    public int getType() {
+        return type;
     }
 
     public String getTitle() {
