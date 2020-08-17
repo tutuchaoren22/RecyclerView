95d9950f8976df19360fb91c5d42cc5e6bc8d9cb
duyuqian <809009017@qq.com>
2020-08-17 12:06:24 +0800

    [duyuqian] feat: add the DataAdapter class

---
+++ app/src/main/java/com/duyuqian/recyclerview/DataAdapter.java
@@ -0,0 +1,56 @@
+package com.duyuqian.recyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> {
+
+    private List<Data> dataList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+        TextView number;
+        TextView description;
+
+        public ViewHolder(View view) {
+            super(view);
+            title = view.findViewById(R.id.title);
+            number = view.findViewById(R.id.number);
+            description = view.findViewById(R.id.description);
+        }
+    }
+
+    public DataAdapter(List<Data> data) {
+        dataList = data;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.data_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Data data = dataList.get(position);
+        holder.title.setText(data.getTitle());
+        holder.number.setText(data.getNumber());
+        holder.description.setText(data.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+
+}
