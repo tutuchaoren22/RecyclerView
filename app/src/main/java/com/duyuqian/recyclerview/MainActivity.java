8580f1f932d5924d451b066e8a2a7b273a470b9c
duyuqian <809009017@qq.com>
2020-08-17 11:26:01 +0800

    [duyuqian] feat: skip to ListActivity when click on button

--- app/src/main/java/com/duyuqian/recyclerview/MainActivity.java
+++ app/src/main/java/com/duyuqian/recyclerview/MainActivity.java
@@ -2,7 +2,12 @@ package com.duyuqian.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_skip_to_list_view)
+    public void onClick() {
+        Intent intent = new Intent(MainActivity.this, ListActivity.class);
+        startActivity(intent);
     }
 }
